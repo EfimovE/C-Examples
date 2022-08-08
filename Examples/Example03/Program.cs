4c36913a628b618dc2ceb224e3c2387961da7fd9
EfimovE <efimov_e@mail.ru>
2022-08-08 17:42:03 +0500

    save 03

--- Examples/Example03/Program.cs
+++ Examples/Example03/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("Введите число.");
+﻿// Console.WriteLine("Введите число.");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int square = num1*num1;
+// Console.Write($"Квадрат числа {num1} равен {square}.");
+
+
+Console.WriteLine("Введите 1-ое число и нажмите Enter.");
 int num1 = Convert.ToInt32(Console.ReadLine());
-int square = num1*num1;
-Console.Write($"Квадрат числа {num1} равен {square}.");
+Console.WriteLine("Введите 2-ое число и нажмите Enter.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num2 == num1*num1)
+    {
+        Console.Write($"Число {num2} является квадратом числа {num1}.");
+    }
+else
+    {
+       Console.Write($"Число {num2} НЕ является квадратом числа {num1}."); 
+    }
