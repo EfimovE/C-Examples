2201d64c10239614cbacdbba306d4b6340ff2f38
EfimovE <efimov_e@mail.ru>
2022-08-08 18:55:20 +0500

    save02

--- Examples/Example04/Program.cs
+++ Examples/Example04/Program.cs
@@ -1,8 +1,19 @@
-﻿Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int count = -num;
-while (count <= num)
+﻿// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int count = -num;
+// while (count <= num)
+// {
+//     Console.Write($"{count}, ");
+//     count++;
+// }
+
+Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 100 && a < 999)
 {
-    Console.Write($"{count}, ");
-    count++;
+    Console.Write($"Последняя цифра числа {a} = {a % 10} ");
+}
+else
+{
+    Console.Write($"Вы ввели НЕ трехзначное число!");
 }
